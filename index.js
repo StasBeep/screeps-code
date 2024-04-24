99855d244e2e59f6f05e3eb670cf778a6e79e162
Stas Oskol <stanislav.osk@mail.ru>
2024-04-24 14:23:33 +0300

    хождение screeps туда-сюда с ресурсами

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Harvester1');
 module.exports.loop = function () {
     // Переменная которая будет держать скрипера под названием 'Harvester1'
     var creep = Game.creeps['Harvester1'];
-    // Переменная которая ищет во всей комнате - источники ресурсов
-    var sources = creep.room.find(FIND_SOURCES);
-    // Если в комнате существует ресурс, то иди к нему
-    if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0]);
+
+    // Если скрип, который переносит не пустой
+    if (creep.store.getFreeCapacity() > 0) {
+        // Переменная которая ищет во всей комнате - источники ресурсов
+        var sources = creep.room.find(FIND_SOURCES);
+        // Если в комнате существует ресурс, то иди к нему
+        if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(sources[0]);
+        }
+    } else {
+        // Если он собрал энергию, то найди Spawn1 и принеси ресурсы
+        if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(Game.spawns['Spawn1']);
+        }
     }
 }
