29358de9bfe018fdf9c3e337536260a335c974c5
Stas Oskol <stanislav.osk@mail.ru>
2024-04-24 14:38:54 +0300

    подключение компонета, который лежит в другом файле

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // Разделение на модульное программирование
+// Переменная (объект), который вытаскивается с файла role.havester, с помощью export
+var roleHarvester = require('role.harvester');
+
+module.exports.loop = function () {
+    // Цикл, который проходит каждый screeps
+    for (var name in Game.creeps) {
+        // Берётся один из screeps
+        var creep = Game.creeps[name];
+        // Вызов функции run и прокидывание screeps(скрипера)
+        roleHarvester.run(creep);
+    }
+}
