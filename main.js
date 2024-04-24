b7bdf99a4e953a0ab69b5482c3708fc607a9ee5c
Stas Oskol <stanislav.osk@mail.ru>
2024-04-24 15:43:03 +0300

    реакция на строителя

--- main.js
+++ main.js
@@ -35,3 +35,19 @@ module.exports.loop = function () {
         }
     }
 }
+
+// Переменная (объект), который вытаскивается с файла role.builder, с помощью export
+var roleBuilder = require('role.builder');
+
+module.exports.loop = function () {
+
+    for (var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if (creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if (creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+}
