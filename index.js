ea0c2cab98e4a6b305bc57a174daabb0ab59ba81
Stas Oskol <stanislav.osk@mail.ru>
2024-04-24 14:33:01 +0300

    автономный код для каждого скрипера на движения и попление казны

--- index.js
+++ index.js
@@ -21,3 +21,26 @@ module.exports.loop = function () {
         }
     }
 }
+
+// Создание второго screeps, под названием Harvester2, для него нужно 200 энергии
+// Если энергии не хватает, то выскакивает ошибка -6
+Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Harvester2');
+
+module.exports.loop = function () {
+    // бегаем по всем скрипам, которые присутствуют в комнате
+    for (var name in Game.creeps) {
+        // берём одного из screeps
+        var creep = Game.creeps[name];
+
+        if (creep.store.getFreeCapacity() > 0) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
+        } else {
+            if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.spawns['Spawn1']);
+            }
+        }
+    }
+}
